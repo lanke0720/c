3a9b0ff3c9f384aec355a9e7d4620c1a05419053
cpp <1600977813@qq.com>
2016-10-10 09:43:55 +0800

    malloc

---
+++ getmemory.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//void GetMemory(char ** p, int num)
+//{
+//	*p = ( char *)malloc(num*sizeof (char));
+//}
+//char* GetMemory(int num)
+//{
+//	char * p = NULL;
+//	p = ( char *)malloc(num*sizeof (char));
+//	return p;
+//}
+//int main()
+//{
+//	char *str = NULL;
+//	str = GetMemory (10);
+//	strcpy(str, "hello");
+//	printf("%s\n", str);
+//	free (str);
+//	return 0;
+//}
