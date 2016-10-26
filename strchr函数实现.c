8e3fa55b529d2bf0dbd9bea690c9ef4061d88ce6
cpp <1600977813@qq.com>
2016-10-26 23:32:34 +0800

    strchr

---
+++ strchr函数实现.c
@@ -0,0 +1,24 @@
+//
+//char *my_strchr(const char*str, int n)
+//{
+//	while(*str)
+//	{	
+//		if(*str == n)
+//			return (char*)str;
+//		str++;
+//	}
+//	return NULL;
+//}
+//int main()
+//{
+//// 	char *p = "abcdef";
+//// 	char *q = "123456";
+//// 	strcpy(p,q);
+//	char *p = "123456";
+//	char *ret = my_strchr(p, '1');
+//	if(ret != NULL)
+//	{
+//		printf("%c\n", *ret);
+//	}
+//	return 0;
+//}
