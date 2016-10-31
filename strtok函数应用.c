14768dcd15556bc88e66653dd73f1a3876e9a5c6
cpp <1600977813@qq.com>
2016-10-31 12:14:39 +0800

    strtok

---
+++ strtok函数应用.c
@@ -0,0 +1,20 @@
+//
+//#include <stdio.h>
+//int main()
+//{
+//	char *p = "192#168.0.1";
+//	char arr[30] = {0};
+//	char *substr = NULL;
+//	strcpy(arr, p);
+//
+//	for(substr=strtok(arr, ".#");
+//		substr != NULL;
+//		substr=strtok(NULL, ".#")
+//		)
+//	{
+//		printf("%s\n", substr);
+//	}
+//
+
+//	return 0;
+//}
